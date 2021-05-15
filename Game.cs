2ee652d0bbca9f5c1ed75af76148e68ca5ab7dcd
oswaldo orellana <oswaldo.orellana.v@gmail.com>
2021-05-15 18:33:29 -0400

    primer paso

---
+++ Game.cs
@@ -0,0 +1,11 @@
+using OpenTK;
+using OpenTK.Graphics;
+namespace grafica
+{
+    public class Game : GameWindow
+    {
+        public Game(int width, int heigth,string title) : base(width,heigth,GraphicsMode.Default,title){
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace grafica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             using (Game game = new Game(800,600,"Silla"))
+             {
+                 game.Run(60.0);
+             }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
